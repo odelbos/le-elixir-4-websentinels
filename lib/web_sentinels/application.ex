6a6f729e75e5d3e17d215f84a242cf8b8e8999b0
odelbos <od@phibox.com>
2022-03-23 00:20:15 +0100

    feat: add settings agent and scheduler sup to application

--- lib/web_sentinels/application.ex
+++ lib/web_sentinels/application.ex
@@ -1,6 +1,4 @@
 defmodule WebSentinels.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,10 @@ defmodule WebSentinels.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: WebSentinels.Worker.start_link(arg)
-      # {WebSentinels.Worker, arg}
+      {WebSentinels.Settings, []},
+      {WebSentinels.Scheduler, []}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: WebSentinels.Supervisor]
     Supervisor.start_link(children, opts)
   end
