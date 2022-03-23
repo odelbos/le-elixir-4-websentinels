64fcc52278228fa0c1e9162a63275adaf305ae6f
odelbos <od@phibox.com>
2022-03-23 13:47:06 +0100

    feat: do the http request and validates status

--- lib/web_sentinels/worker.ex
+++ lib/web_sentinels/worker.ex
@@ -1,6 +1,7 @@
 defmodule WebSentinels.Worker do
   require Logger
   use GenServer
+  alias WebSentinels.Expectations
 
   def start_link(state) do
     name = worker_name state
@@ -25,11 +26,41 @@ defmodule WebSentinels.Worker do
   end
 
   @impl true
-  def handle_info(:run, state) do
-    #
-    # TODO : Do the http ping
-    #
-    Logger.debug "Run: #{worker_name state}, url: #{state[:url]}"
+  def handle_info(:run, %{url: url} = state) do
+    Logger.debug "Run: #{worker_name state}, url: #{url}"
+
+    opts = [timeout: 5000, recv_timeout: 5000, follow_redirect: false]
+    response = HTTPoison.get url, [], opts
+
+    case response do
+      {:ok, %HTTPoison.Response{body: body, status_code: status, headers: headers}} ->
+        errors =
+          Expectations.validates state[:expects], status, body, headers
+
+        if length(errors) == 0 do
+          Logger.info "#{worker_name state}: ok, url: #{url}"
+          #
+          # TODO : log success
+          #
+        else
+          Logger.info "#{worker_name state}: expectations failed, url: #{url}"
+          IO.inspect errors
+          #
+          # TODO : Send Pushover alert
+          #
+        end
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        # reason -> :nxdomain, :timeout
+        Logger.warn "#{worker_name state}: Error, #{reason}"
+        #
+        # TODO : Send Pushover alert
+        #
+      _ ->
+        Logger.warn "#{worker_name state}: unknown error"
+        #
+        # TODO : Send Pushover alert
+        #
+    end
 
     {:noreply, state}
   end
